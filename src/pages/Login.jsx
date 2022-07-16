d70ade991c6445038d3136bf7daa3e5513ea3488
Luis Henrique <senseyluiz@gmail.com>
2022-07-16 11:04:22 -0300

    Criando contexto e iniciando page login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import logo from "../img/xp-investimentos-logo-4.png"
 
 function Login () {
   return(
     <main>
-      <h1>Login</h1>
+      <form action="">
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <Link to='/ListaAcoes' >  <button type="submit"> Acessar </button> </Link>
+      </form>
     </main>
   )
 }
